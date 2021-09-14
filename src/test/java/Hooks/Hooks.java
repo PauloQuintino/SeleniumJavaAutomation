e16c697de891e933236602744633d92d1f1640cc
PauloQuintino <pauloqfs16@gmail.com>
2021-09-14 12:04:17 -0300

    Alterado chromedriver para versão 93 e baseUrl sendo pega direto da variavel de ambiente da máquina onde é executado

--- src/test/java/Hooks/Hooks.java
+++ src/test/java/Hooks/Hooks.java
@@ -1,5 +1,6 @@
 package Hooks;
 
+import Core.Configuration;
 import datafiles.TestDataReader;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -7,6 +8,9 @@ import io.cucumber.java.Scenario;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static Core.DriverFactory.getDriver;
 import static Core.DriverFactory.killDriver;
@@ -22,7 +26,31 @@ public class Hooks {
         BasicConfigurator.configure();
         PropertyConfigurator.configure("Log4j.properties");
         LOG.info("SET UP AUTOMATION");
-        getDriver().get("http://automationpractice.com/index.php?");
+
+
+        //creating a map to insert the environment variables
+        Map<String, String> configVars = new HashMap<String, String>(){
+            {
+                //add to map URL_PROJECT with same name of OS environment variable
+                put("baseUrl", "URL_PROJECT");
+            }
+        };
+
+        Configuration config = Configuration.getInstance();
+
+        //iterating the Map to insert System Environment Variables into config baseUrl
+        for(Map.Entry<String, String> entry : configVars.entrySet()){
+            if(System.getenv(entry.getValue()) != null){
+                config.setProperty(entry.getKey(), System.getenv(entry.getValue()));
+            }
+        }
+
+        if(config.hasProperty("baseUrl")){
+//            getDriver().get("http://automationpractice.com/index.php?");
+            getDriver().get(Configuration.getInstance().getProperty("baseUrl"));
+        }
+
+
         LOG.info("\n========================\nFeature name: " + scenario.getName() + "\n========================");
 
         Object[] arrayTags = scenario.getSourceTagNames().toArray();
